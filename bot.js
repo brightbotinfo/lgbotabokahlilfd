18bbbbfdc87537524d6a2d6a089640aec9792fb3
brightbotinfo <41553852+brightbotinfo@users.noreply.github.com>
2018-08-10 01:42:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -821,22 +821,6 @@ client.on('message', message => {
 }); 
 
 
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('discord.gg')){
-        message.delete()	 
-   message.channel.sendMessage("", {embed: {
-        title: "لا تنشر",
-        color: 0x06DF00,
-        description: "يمنع النشر في هذا السيرفر",
-        footer: {
-          text: "By Abo Khalil"
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
-
-     
-}); 
 
 
 
